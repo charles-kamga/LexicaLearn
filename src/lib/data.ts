5f3bca997e9908b5fc8f313bfc303248171e09f8
Charles KAMGA <Charleskamga27@gmail.com>
2025-10-26 14:58:26 +0000

    Je veux que tu implementes ces lecņs : 🧱 1. Les bases structurelles
    Al

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,6 +2,54 @@ import type { Lesson, VocabularyItem, ProgressChartData } from '@/lib/types';
 
 export const lessonsData: Lesson[] = [
   {
+    id: 'personal-pronouns-and-to-be',
+    title: 'Personal Pronouns & "to be"',
+    description: 'Learn about subject pronouns and the verb "to be".',
+    imageUrl: '5',
+    content: `
+<h3>Personal Pronouns</h3>
+<p>Personal pronouns replace nouns. The most common are subject pronouns:</p>
+<ul>
+  <li><b>I</b> - (First person singular)</li>
+  <li><b>You</b> - (Second person singular/plural)</li>
+  <li><b>He</b> - (Third person singular, masculine)</li>
+  <li><b>She</b> - (Third person singular, feminine)</li>
+  <li><b>It</b> - (Third person singular, neuter/object)</li>
+  <li><b>We</b> - (First person plural)</li>
+  <li><b>They</b> - (Third person plural)</li>
+</ul>
+
+<h3>The Verb "to be"</h3>
+<p>The verb "to be" is one of the most important verbs. Its form changes with the pronoun.</p>
+<p><b>Present Simple of "to be"</b></p>
+<ul>
+  <li>I <b>am</b></li>
+  <li>You <b>are</b></li>
+  <li>He/She/It <b>is</b></li>
+  <li>We <b>are</b></li>
+  <li>They <b>are</b></li>
+</ul>
+<p><b>Example:</b> She <b>is</b> a doctor. They <b>are</b> happy.</p>
+    `,
+    quiz: [
+      {
+        question: '___ are students.',
+        options: ['We', 'He', 'She'],
+        correctAnswer: 'We',
+      },
+      {
+        question: 'Mark is an engineer. ___ is very smart.',
+        options: ['She', 'It', 'He'],
+        correctAnswer: 'He',
+      },
+       {
+        question: 'The cat is sleeping. ___ is on the sofa.',
+        options: ['He', 'She', 'It'],
+        correctAnswer: 'It',
+      },
+    ],
+  },
+  {
     id: 'present-simple',
     title: 'Present Simple Tense',
     description: 'Learn how to use the present simple tense for habits, routines, and general truths.',
@@ -52,6 +100,102 @@ The **Present Simple** tense is one of the most fundamental tenses in English. W
         question: 'Choose the correct question form.',
         options: ['Do you likes ice cream?', 'Does you like ice cream?', 'Do you like ice cream?', 'Does you likes ice cream?'],
         correctAnswer: 'Do you like ice cream?',
+      },
+    ],
+  },
+  {
+    id: 'present-continuous',
+    title: 'Present Continuous',
+    description: 'For actions happening right now.',
+    imageUrl: '6',
+    content: `
+<h3>Present Continuous</h3>
+<p>We use the Present Continuous for actions happening at the moment of speaking.</p>
+<p><b>Structure:</b> Subject + am/is/are + Verb-ing</p>
+<ul>
+  <li><b>Positive:</b> I <b>am reading</b> a book.</li>
+  <li><b>Negative:</b> She <b>is not (isn't) sleeping</b>.</li>
+  <li><b>Question:</b> <b>Are</b> you <b>working</b>?</li>
+</ul>
+<p><b>Use cases:</b></p>
+<ul>
+    <li>Actions happening now: <i>Look! The baby <b>is smiling</b>.</i></li>
+    <li>Temporary situations: <i>He <b>is staying</b> with his friend for a week.</i></li>
+    <li>Future plans (informal): <i>We <b>are meeting</b> at 6 PM.</i></li>
+</ul>
+    `,
+    quiz: [
+      {
+        question: 'What ___ you doing?',
+        options: ['are', 'is', 'am'],
+        correctAnswer: 'are',
+      },
+      {
+        question: 'She ___ to music right now.',
+        options: ['listens', 'is listening', 'listen'],
+        correctAnswer: 'is listening',
+      },
+    ],
+  },
+  {
+    id: 'past-simple',
+    title: 'Past Simple',
+    description: 'For finished actions in the past.',
+    imageUrl: '7',
+    content: `
+<h3>Past Simple</h3>
+<p>The Past Simple tense is used for actions that started and finished at a specific time in the past.</p>
+<p><b>Structure (Regular Verbs):</b> Subject + Verb-ed</p>
+<p><b>Structure (Irregular Verbs):</b> Subject + 2nd form of verb</p>
+<ul>
+  <li><b>Positive:</b> I <b>visited</b> Paris last year. / She <b>went</b> to the cinema.</li>
+  <li><b>Negative:</b> They <b>did not (didn't) finish</b> the project.</li>
+  <li><b>Question:</b> <b>Did</b> you <b>see</b> that movie?</li>
+</ul>
+    `,
+    quiz: [
+      {
+        question: 'I ___ to the store yesterday.',
+        options: ['go', 'goed', 'went'],
+        correctAnswer: 'went',
+      },
+      {
+        question: '___ you finish your homework?',
+        options: ['Did', 'Do', 'Have'],
+        correctAnswer: 'Did',
+      },
+    ],
+  },
+  {
+    id: 'future-tenses',
+    title: 'Future: Will vs. Going to',
+    description: 'Learn the difference between "will" and "going to".',
+    imageUrl: '8',
+    content: `
+<h3>Future Tenses</h3>
+<p>Both "will" and "be going to" are used to talk about the future, but in different contexts.</p>
+<p><b>Using "will"</b></p>
+<ul>
+  <li>For spontaneous decisions: <i>I think I <b>will have</b> the salad.</i></li>
+  <li>For promises or offers: <i>I <b>will help</b> you with your bags.</i></li>
+  <li>For predictions based on opinion: <i>I think it <b>will rain</b> tomorrow.</i></li>
+</ul>
+<p><b>Using "be going to"</b></p>
+<ul>
+  <li>For future plans and intentions: <i>I <b>am going to visit</b> my grandparents next week.</i></li>
+  <li>For predictions based on present evidence: <i>Look at those clouds! It's <b>going to rain</b>.</i></li>
+</ul>
+    `,
+    quiz: [
+      {
+        question: 'A: The phone is ringing! B: I ___ get it!',
+        options: ['am going to', 'will'],
+        correctAnswer: 'will',
+      },
+      {
+        question: 'We have tickets for the concert. We ___ see Taylor Swift next month!',
+        options: ['will', 'are going to'],
+        correctAnswer: 'are going to',
       },
     ],
   },
@@ -172,6 +316,54 @@ Use **in** for months, years, centuries and long periods.
     ],
   },
   {
+    id: 'prepositions-of-place',
+    title: 'Prepositions of Place',
+    description: 'Learn "in", "on", and "at" for locations.',
+    imageUrl: '9',
+    content: `
+<h3>Prepositions of Place</h3>
+<p>"In", "on", and "at" are also used for locations.</p>
+<p><b>In:</b> Used for enclosed spaces or larger areas.</p>
+<ul>
+  <li>in a box</li>
+  <li>in a room</li>
+  <li>in a country (e.g., in France)</li>
+  <li>in a city (e.g., in London)</li>
+</ul>
+<p><b>On:</b> Used for surfaces.</p>
+<ul>
+  <li>on the table</li>
+  <li>on the wall</li>
+  <li>on the floor</li>
+  <li>on a page</li>
+</ul>
+<p><b>At:</b> Used for specific points or locations.</p>
+<ul>
+  <li>at the bus stop</li>
+  <li>at the door</li>
+  <li>at the top of the page</li>
+  <li>at the end of the street</li>
+</ul>
+    `,
+    quiz: [
+      {
+        question: 'The book is ___ the table.',
+        options: ['in', 'on', 'at'],
+        correctAnswer: 'on',
+      },
+      {
+        question: 'I live ___ Paris.',
+        options: ['in', 'on', 'at'],
+        correctAnswer: 'in',
+      },
+       {
+        question: 'Wait for me ___ the bus stop.',
+        options: ['in', 'on', 'at'],
+        correctAnswer: 'at',
+      },
+    ],
+  },
+  {
     id: 'conditionals',
     title: 'Conditionals (If Clauses)',
     description: 'Understand and use conditional sentences.',
@@ -218,6 +410,36 @@ Used for a hypothetical situation in the past that did not happen.
         question: 'She would have come if she ___ invited.',
         options: ['was', 'had been', 'has been', 'would be'],
         correctAnswer: 'had been',
+      },
+    ],
+  },
+  {
+    id: 'modal-verbs',
+    title: 'Modal Verbs',
+    description: 'Learn to use can, could, must, should, etc.',
+    imageUrl: '10',
+    content: `
+<h3>Modal Verbs</h3>
+<p>Modal verbs are auxiliary verbs that express necessity, possibility, permission, or ability.</p>
+<ul>
+  <li><b>Can:</b> Ability / Permission (informal) - <i>I <b>can</b> swim. <b>Can</b> I go?</i></li>
+  <li><b>Could:</b> Past ability / Polite request - <i>I <b>could</b> run fast. <b>Could</b> you help me?</i></li>
+  <li><b>Must:</b> Obligation / Certainty - <i>You <b>must</b> stop. He <b>must</b> be tired.</i></li>
+  <li><b>Should:</b> Advice / Recommendation - <i>You <b>should</b> see a doctor.</i></li>
+  <li><b>May:</b> Permission (formal) / Possibility - <i><b>May</b> I enter? It <b>may</b> rain.</i></li>
+  <li><b>Might:</b> Possibility (less certain than may) - <i>He <b>might</b> come later.</i></li>
+</ul>
+    `,
+    quiz: [
+      {
+        question: 'You ___ finish your homework before you go out.',
+        options: ['can', 'must', 'might'],
+        correctAnswer: 'must',
+      },
+      {
+        question: '___ I borrow your pen, please?',
+        options: ['Must', 'Could', 'Should'],
+        correctAnswer: 'Could',
       },
     ],
   },
